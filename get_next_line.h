4802e1d24d13c03769e05d5e03759ceac7cb3fe3
Alexis PUTMAN <aputman@e1r4p16.42.fr>
2016-04-21 18:18:09 +0200

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aputman <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/14 16:49:25 by aputman           #+#    #+#             */
+/*   Updated: 2016/04/11 14:00:05 by aputman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <fcntl.h>
+# define BUFF_SIZE 64
+
+int				get_next_line(const int fd, char **line);
+
+#endif
